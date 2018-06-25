a6424d72af0d9eadf720d0ead3e65d067cb3b851
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-25 17:32:27 +0900

    イニシャライザに関する実装

--- MemoReminder/MemoReminder/ViewController.swift
+++ MemoReminder/MemoReminder/ViewController.swift
@@ -18,6 +18,22 @@ class ViewController: UIViewController {
     let footerView: UIView
     let newButton: UIButton
     let countLabel: UILabel
+    
+    // イニシャライザ
+    init() {
+        self.tableView = UITableView.newAutoLayoutView()
+        self.footerView = UIView.newAutoLayoutView()
+        self.newButton = UIButton(type: .system)
+        // AutoLayout解除
+        self.newButton.translatesAutoresizingMaskIntoConstraints = false
+        self.countLabel = UILabel.newAutoLayoutView()
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
