c51a1713ffbc009c461bc0053d6f4d025be70a86
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-28 13:18:05 +0900

    構造体Noteの実装

---
+++ MemoReminder/MemoReminder/Components/note.swift
@@ -0,0 +1,14 @@
+//
+//  note.swift
+//  MemoReminder
+//
+//  Created by home on 2018/06/28.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+struct Note {
+    var id: String = ""
+    var memo: String = ""
+}
