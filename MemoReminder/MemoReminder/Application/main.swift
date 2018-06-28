bb8ad12ccb9b5104bf15a864d941269237f25c65
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-28 14:33:24 +0900

    main関数の実装

--- MemoReminder/MemoReminder/Application/main.swift
+++ MemoReminder/MemoReminder/Application/main.swift
@@ -9,4 +9,18 @@
 import Foundation
 import UIKit
 
-
+if NSClassFromString("XCTestCase") != nil {
+    UIApplicationMain(
+        Process.argc,
+        Process.unsafeArgv,
+        NSStringFromClass(UIApplication),
+        NSStringFromClass(TestingAppDelegate)
+    )
+} else {
+    UIApplicationMain(
+        Process.argc,
+        Process.unsafeArgv,
+        NSStringFromClass(UIApplication),
+        NSStringFromClass(AppDelegate)
+    )
+}
